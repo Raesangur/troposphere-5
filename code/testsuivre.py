a18b8fc3382151da39d4c4e3f948c5d545eac4ce
raesangur <pascalemmanuel.lachance@gmail.com>
2023-03-15 15:41:07 -0400

    almost suiving the ligne

--- code/testsuivre.py
+++ code/testsuivre.py
@@ -118,35 +118,68 @@ def start_bouge():
                 start_moving(lf = Line_Follower())    
             time.sleep(0.2)
     
-def start_moving(lf):
+def start_moving(lf, vitesseBase = 100):
     lState = ""
+    angle = 90
     while lf.read_digital() != [True, True, True, True, True]:
         rd = lf.read_digital()
         #print(rd)
+
+        if rd == [False, True, True, False, False] or rd == [False, False, True, True, False]:
+            angle = 10
+            vitesseBase = 90
+        if rd == [False, True, False, False, False] or rd == [False, False, False, True, False]:
+            angle = 20
+            vitesseBase = 80
+        if rd == [True, True, False, False, False] or rd == [False, False, False, True, True]:
+            angle = 30
+            vitesseBase = 75
+        if rd == [True, False, False, False, False] or rd == [False, False, False, False, True]:
+            angle = 45
+            vitesseBase = 60
+
+        if rd == [False, False, True, False, False]:
+            angle = 90
+        if rd == [False, False, True, True, False] or rd == [False, False, False, True, False] or rd == [False, False, False, True, True] or rd == [False, False, False, False, True]:
+            angle = 90 + angle
+        if rd == [False, True, True, False, False] or rd == [False, True, False, False, False] or rd == [True, True, False, False, False] or rd == [True, False, False, False, False]:
+            angle = 90 - angle
+        else:
+            print("wtf")
+
+        fw.turn(angle)
+        bw.speed = vitesseBase * 0.3
+        bw.backward()
+        time.sleep(0.01)
+        
+        continue
         if rd == [False, False, True, False, False]:
             if lState != "S" :
+                if lState == "L" or lState == "R":
+                    print("conversion therapy")
+                    time.sleep(0.5)
                 print("straight")
                 fw.turn_straight()
-                bw.speed = 30
+                bw.speed = vitesseBase * 0.5
                 bw.backward()
                 lState ="S"
-                time.sleep(1)
+                time.sleep(0.1)
         elif rd == [False, True, True, False, False] or rd == [False, True, False, False, False] or rd == [True, True, False, False, False] or rd == [True, False, False, False, False] :
             if lState != "L" :
                 print("left")
                 fw.turn_left()
-                bw.speed = 30
+                bw.speed = vitesseBase * 0.3
                 bw.backward()
                 lState = "L"
-                time.sleep(1) 
+                time.sleep(0.1) 
         elif rd == [False, False, True, True, False] or rd == [False, False, False,True, False] or rd == [False, False, False, True, True] or rd == [False, False, False, False, True]:
             if lState != "R" :
                 print("right")
                 fw.turn_right()
-                bw.speed = 30
+                bw.speed = int(vitesseBase * 0.3)
                 bw.backward()
                 lState = "R"
-                time.sleep(1)
+                time.sleep(0.1)
     print("Found line")
     stop()
    
