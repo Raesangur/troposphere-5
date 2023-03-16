8c6bc21ee71d89563e63f40d3033f317accebaf5
anthopop123 <72212667+anthopop123@users.noreply.github.com>
2023-03-15 22:20:52 -0400

    update frouf file in folders

--- Blender.py
+++
@@ -1,5 +0,0 @@
-import bpy
-from bpy import context as C
-from bpy import data as D
-
-print("Started !")
---
+++ Math/Blender.py
@@ -0,0 +1,31 @@
+import bpy
+from bpy import context as C
+from bpy import data as D
+import time
+print("Started !")
+
+bpy.data.objects["PiCar-Chassis-Assembly"].select_set(True)
+ 
+leChar = bpy.context.active_object
+leChar.location.x = 0
+##bpy.ops.screen. change_frame(frame=0.0,snap=True)
+#bpy.data.scenes['Scene'].frame_set(0)
+
+bpy.data.scenes['Scene'].frame_set(0)
+leChar.location.x = 0
+bpy.ops.anim.keyframe_insert(type="LocRotScale")    
+
+frames = [1,900]
+for i in frames:
+    current_frame = frames[i] 
+    bpy.data.scenes['Scene'].frame_set(current_frame)
+    leChar.location.x = (i * i)  * 150
+    bpy.ops.anim.keyframe_insert(type="LocRotScale")    
+#   bpy.context.scene.frame_set(i)     
+#    
+#    
+
+
+bpy.ops.screen.animation_play()
+
+#leChar.location.x = 0
