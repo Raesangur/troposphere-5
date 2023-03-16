7c9261168074b5acf3a27b419654cecd20b5a251
raesangur <pascalemmanuel.lachance@gmail.com>
2023-03-16 15:32:10 -0400

    Saving due to loss of power

--- code/CodeTest/testSuivre.py
+++ code/CodeTest/testSuivre.py
@@ -37,64 +37,37 @@ def start_moving(lf, vitesseBase = 100):
             vitesseBase = 90
         if rd == [False, True, False, False, False] or rd == [False, False, False, True, False]:
             angle = 20
-            vitesseBase = 80
+            vitesseBase = 85
         if rd == [True, True, False, False, False] or rd == [False, False, False, True, True]:
             angle = 30
-            vitesseBase = 75
-        if rd == [True, False, False, False, False] or rd == [False, False, False, False, True]:
+            vitesseBase = 80
+        if rd == [True, False, False, False, False] or rd == [False, False, False, False, True] or rd == [True, True, True, False, False] or rd == [False, False, True, True, True]:
             angle = 45
-            vitesseBase = 60
+            vitesseBase = 75
 
         if rd == [False, False, True, False, False]:
             angle = 90
-        elif rd == [False, False, True, True, False] or rd == [False, False, False, True, False] or rd == [False, False, False, True, True] or rd == [False, False, False, False, True]:
+        elif rd == [False, False, True, True, False] or rd == [False, False, False, True, False] or rd == [False, False, False, True, True] or rd == [False, False, False, False, True] or rd == [False, False, True, True, True]:
             angle = 90 + angle
-        elif rd == [False, True, True, False, False] or rd == [False, True, False, False, False] or rd == [True, True, False, False, False] or rd == [True, False, False, False, False]:
+        elif rd == [False, True, True, False, False] or rd == [False, True, False, False, False] or rd == [True, True, False, False, False] or rd == [True, False, False, False, False] or rd == [True, True, True, False, False]:
             angle = 90 - angle
         else:
             WTF = True
-            vitesseBase = 90
-            print("wtf")
-
-        if WTF == True:
+            print("Continue")
+            
+        if WTF == True :
+            fw.turn_straight()
+            bw.speed = (65*0.4)
             bw.forward()
-            bw.speed = int(vitesseBase * 0.3)
             time.sleep(0.5)
             WTF = False
-        else:
+        else :        
             fw.turn(angle)
-            bw.speed = int(vitesseBase * 0.3)
+            bw.speed = int(vitesseBase * 0.4)
             bw.backward()
             time.sleep(0.01)
+
         
-        continue
-        if rd == [False, False, True, False, False]:
-            if lState != "S" :
-                if lState == "L" or lState == "R":
-                    print("conversion therapy")
-                    time.sleep(0.5)
-                print("straight")
-                fw.turn_straight()
-                bw.speed = vitesseBase * 0.5
-                bw.backward()
-                lState ="S"
-                time.sleep(0.1)
-        elif rd == [False, True, True, False, False] or rd == [False, True, False, False, False] or rd == [True, True, False, False, False] or rd == [True, False, False, False, False] :
-            if lState != "L" :
-                print("left")
-                fw.turn_left()
-                bw.speed = vitesseBase * 0.3
-                bw.backward()
-                lState = "L"
-                time.sleep(0.1) 
-        elif rd == [False, False, True, True, False] or rd == [False, False, False,True, False] or rd == [False, False, False, True, True] or rd == [False, False, False, False, True]:
-            if lState != "R" :
-                print("right")
-                fw.turn_right()
-                bw.speed = int(vitesseBase * 0.3)
-                bw.backward()
-                lState = "R"
-                time.sleep(0.1)
     print("Found line")
     stop()
    
